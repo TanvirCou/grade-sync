67258e2f567be3c4f3259a8f77a00e19de8edc26
Tanvir Ahmed <ahmed.tnvr999@gmail.com>
2025-03-07 02:35:27 +0600

    Metadata related bug fixed.

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,13 +1,7 @@
 'use client';
 import { SignIn, useUser } from '@clerk/nextjs';
-import { Metadata } from 'next';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
-
-export const metadata: Metadata = {
-  title: 'grade-sync | sign-in',
-  description: 'This is a school management webapp',
-};
 
 export default function Page() {
   const { user } = useUser();
